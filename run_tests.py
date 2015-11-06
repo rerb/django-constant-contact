6f86d3756ec2bcc1a73372682ed26e1035b64dcf
Benjamin Stookey <ben.stookey@gmail.com>
2015-11-06 10:33:48 -0500

    PEP8 love.

--- run_tests.py
+++ run_tests.py
@@ -4,10 +4,11 @@ import django
 
 BASE_PATH = os.path.dirname(__file__)
 
+
 def main():
     """
     Standalone django model test with a 'memory-only-django-installation'.
-    You can play with a django model without a complete django app installation.
+    You can play with a django model without a complete django app installation
     http://www.djangosnippets.org/snippets/1044/
     """
     sys.exc_clear()
@@ -21,7 +22,7 @@ def main():
         'django.contrib.contenttypes',
         'django_constant_contact',
     )
-    if django.VERSION > (1,2):
+    if django.VERSION > (1, 2):
         global_settings.DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
@@ -45,13 +46,20 @@ def main():
     )
 
     # custom settings for tests
-    global_settings.CONSTANT_CONTACT_API_KEY = os.environ.get('CONSTANT_CONTACT_API_KEY', None)
-    global_settings.CONSTANT_CONTACT_ACCESS_TOKEN = os.environ.get('CONSTANT_CONTACT_ACCESS_TOKEN', None)
-    global_settings.CONSTANT_CONTACT_FROM_EMAIL = os.environ.get('CONSTANT_CONTACT_FROM_EMAIL', None)
-    global_settings.CONSTANT_CONTACT_REPLY_TO_EMAIL = os.environ.get('CONSTANT_CONTACT_REPLY_TO_EMAIL', None)
-    global_settings.CONSTANT_CONTACT_USERNAME = os.environ.get('CONSTANT_CONTACT_USERNAME', None)
-    global_settings.CONSTANT_CONTACT_PASSWORD = os.environ.get('CONSTANT_CONTACT_PASSWORD', None)
-    global_settings.MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY', None)
+    global_settings.CONSTANT_CONTACT_API_KEY = os.environ.get(
+        'CONSTANT_CONTACT_API_KEY', None)
+    global_settings.CONSTANT_CONTACT_ACCESS_TOKEN = os.environ.get(
+        'CONSTANT_CONTACT_ACCESS_TOKEN', None)
+    global_settings.CONSTANT_CONTACT_FROM_EMAIL = os.environ.get(
+        'CONSTANT_CONTACT_FROM_EMAIL', None)
+    global_settings.CONSTANT_CONTACT_REPLY_TO_EMAIL = os.environ.get(
+        'CONSTANT_CONTACT_REPLY_TO_EMAIL', None)
+    global_settings.CONSTANT_CONTACT_USERNAME = os.environ.get(
+        'CONSTANT_CONTACT_USERNAME', None)
+    global_settings.CONSTANT_CONTACT_PASSWORD = os.environ.get(
+        'CONSTANT_CONTACT_PASSWORD', None)
+    global_settings.MAILCHIMP_API_KEY = os.environ.get(
+        'MAILCHIMP_API_KEY', None)
 
     global_settings.SECRET_KEY = "blahblah"
 
@@ -60,11 +68,11 @@ def main():
 
     # import pdb; pdb.set_trace()
 
-    if django.VERSION > (1,2):
+    if django.VERSION > (1, 2):
         test_runner = test_runner()
-        failures = test_runner.run_tests(['django_constant_contact',])
+        failures = test_runner.run_tests(['django_constant_contact', ])
     else:
-        failures = test_runner(['django_constant_contact',], verbosity=1)
+        failures = test_runner(['django_constant_contact', ], verbosity=1)
     sys.exit(failures)
 
 if __name__ == '__main__':
